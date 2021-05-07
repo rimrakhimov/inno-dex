86745c83adbf2c4ec63ddc0c49f927054f5a9abd
Rim Rakhimov <rimrakhimov@gmail.com>
2021-05-07 17:40:13 +0300

    Modified function to request tokens

--- contracts/ERC20/extensions/ERC20Mintable.sol
+++ contracts/ERC20/extensions/ERC20Mintable.sol
@@ -6,13 +6,16 @@ import "../../utils/Context.sol";
 pragma solidity ^0.8.3;
 
 contract ERC20Mintable is Context, ERC20 {
+    uint256 public constant distanceLimit = 30;
+
+    event Granted(address indexed receiver, uint256 tokens);
+
     constructor(string memory name_, string memory symbol_)
         ERC20(name_, symbol_)
     {}
 
-    event Granted(address indexed receiver, uint256 tokens);
-
-    function requestTokens() public returns (bool) {
+    function requestTokens(uint256 distance) public returns (bool) {
+        require(distance <= distanceLimit, "Too far away");
         uint256 toMint = 1;
 
         _mint(_msgSender(), toMint);
